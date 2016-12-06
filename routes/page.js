36bc9ce69222d296f54b9eacff5a93d9ca455393
zitim <s911297@gmail.com>
2016-12-06 11:25:14 +0800

    Add files via upload

--- routes/page.js
+++ routes/page.js
@@ -1,118 +1,90 @@
 //首頁
-
-exports.index = function(req, res) {
-        
-       
-            var mongodb = require('mongodb');
-            var mc = mongodb.MongoClient;
-            mc.connect('mongodb://:@ds115738.mlab.com:15738/messageboard', (err,db) => {
-            var collection = db.collection('test2');
-    //查詢
-            collection.find().toArray((err, result) => {
-                if(!err){
-                    
-                    for (var i = 0; i < result.length ; i--) {
-                        //data[i] = result[i];
-                        res.render('pages/index', {
-                            listdata: result
-                        });
-                    }
-  
-                }else{
-                    console.log(err);
-                }
-            });
-           
-
-            //結束
-            db.close()
-            });
-};
-
-exports.delete = function(req, res) {
-
-}
-
-/*exports.index = function(req, res) {
-    var mongodb = require('mongodb');
-    var mc = mongodb.MongoClient;
-
-    mc.connect('mongodb://zitim:999TIMTI@ds115738.mlab.com:15738/messageboard', (err,db) => {
-    var collection = db.collection('test2');
-
-    //查詢
-    //var condition = {title: '324'};
-    collection.find().toArray((err, result) => {
-      if(!err){
-        for (var i = result.length - 1; i >= 0; i--) {
-          var data = result[i];
-            res.render('pages/index', {
-            ogheadTitle: '留言板',
-            listdata: data
-        });
-        }
-          //console.log(result);
-      }else{
-          console.log(err);
-      }
-  });
-
-
-  //結束
-  db.close()
-
-
-  
-
-});
-    
-};*/
-
-//傳統輸入 
-
-exports.post = function(req, res) {
-    console.log(req.body);
-    res.render('pages/success');
-
 var mongodb = require('mongodb');
+var ObjectId = require('mongodb').ObjectID;
 var mc = mongodb.MongoClient;
 
-mc.connect('mongodb://:@ds115738.mlab.com:15738/messageboard', (err,db) => {
-    var collection = db.collection('test2');
-    var Today = new Date();
-    var str=(Today.getFullYear()+ " 年 " + (Today.getMonth()+1) + " 月 " + Today.getDate() + " 日");
-    console.log(str);
-    //新增資料
-    var data = {
-        title: str,
-        message: req.body.password,
-    };
-    collection.insert(data, (err, result) => {
-        if(!err){
-            console.log(result);
-        }else{
-            console.log(err);
-        }
+//列出資料
+exports.index = function(req, res) {
+    mc.connect('mongodb://:@ds115738.mlab.com:15738/messageboard', (err,db) => {
+        var collection = db.collection('test2');
+
+        collection.find().toArray((err, result) => {
+            if(!err){
+                res.render('pages/index', {
+                         listdata: result
+                });
+                // for (var i = 0; i < result.length ; i++) {
+                //     //data[i] = result[i];
+                //     res.render('pages/index', {
+                //         listdata: data
+                //     });
+                // }
+            }else{
+                console.log(err);
+            }
+        });    
+        db.close()  
     });
+    
+};
 
-    //結束
-    db.close()
+//傳統輸入 
+exports.post = function(req, res) {
+    //console.log(req.body);
+    //res.render('pages/success');
 
+    mc.connect('mongodb://:@ds115738.mlab.com:15738/messageboard', (err,db) => {
+        var collection = db.collection('test2');
+        var Today = new Date();
+        var str=(Today.getFullYear()+ " 年 " + (Today.getMonth()+1) + " 月 " + Today.getDate() + " 日");
+        console.log(str);
+    //新增資料
+        var data = {
+            title: str,
+            message: req.body.password,
+        };
+        collection.insert(data, (err, result) => {
+            if(!err){
+                console.log(result);
+            }else{
+                console.log(err);
+            }
+        });
 
-  
-
-});
+        //結束
+        db.close()
+    });
     //res.send(req.body.password);
 };
 
+//刪除
+exports.delete = function(req, res) {
+    
+    mc.connect('mongodb://:@ds115738.mlab.com:15738/messageboard', (err,db) => {
+    var collection = db.collection('test2');
+
+    console.log(req.body.id);
+    var condition = {"_id": ObjectId(req.body.id)};
+    collection.remove(condition, (err, result) => {
+        if(!err){
+            res.send('success');
+        }else{
+            res.send('error');
+        }
+    });
+
+    db.close()
+    });
+
+}
+
 exports.postAjax = function(req, res) {
     // ajax
-   if (req.body.password == 1234) {
+    if (req.body.password == 1234) {
         res.send('success');
     } else(
         res.send('error')
     );
-
 };
 
 //get取得資料
